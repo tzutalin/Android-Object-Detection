58cd85ff70f87c0a73ccd445a626366a28c1de8d
TzuTa Lin <tzu.ta.lin@gmail.com>
2015-10-14 16:56:47 +0800

    Add Andriod libraray module

---
+++ cnnlibs/src/main/java/com/tzutalin/vision/visionrecognition/SceneClassifier.java
@@ -0,0 +1,156 @@
+/*
+*  Copyright (C) 2015 TzuTaLin
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package com.tzutalin.vision.visionrecognition;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Identifies the current scene in a
+ * {@link android.graphics.Bitmap} graphic object.
+ */
+public class SceneClassifier extends CaffeClassifier<List<VisionDetRet>> {
+    private static final int MODEL_DIM = 224;
+    private ByteBuffer _handler = null;
+
+    /**
+     * Creates a SceneClassifier, configured with its model path, trained weights, etc.
+     * These parameters cannot be changed once the object is constructed.
+     *
+     * @param context          Context
+     * @param sceneModelPath   Caffe's model
+     * @param sceneWieghtsPath Caffe's trained wieght
+     * @param sceneManefile    The file path of the image image
+     * @param sceneSynsetFile  The file path to load label's titles
+     */
+    public SceneClassifier(Context context, String sceneModelPath, String sceneWieghtsPath, String sceneManefile, String sceneSynsetFile) throws IllegalAccessException {
+        super(context, sceneModelPath, sceneWieghtsPath, sceneManefile, sceneSynsetFile);
+        if (new File(mModelPath).exists() == false ||
+                new File(mWeightsPath).exists() == false ||
+                new File(mSynsetPath).exists() == false ) {
+            throw new IllegalAccessException(" Cannot find model");
+        }
+    }
+
+    /**
+     * Recognize the scene according to the given image path
+     *
+     * @param imgPath image path
+     * @return The list of the result {@link VisionDetRet} which scenes are recognized
+     * @throws IllegalArgumentException if the Bitmap dimensions don't match
+     *                                  the dimensions defined at initialization or the given array
+     *                                  is not sized equal to the width and hegiht defined at initialization
+     */
+    @Override
+    public List<VisionDetRet> classifyByPath(String imgPath) {
+        float[] propArray = jniClassifyImgByPath(imgPath);
+        List<VisionDetRet> ret = new ArrayList<VisionDetRet>();
+
+        if (propArray != null) {
+            Map<String, Float> sortedmap = Utils.sortPrediction(mSynsets, propArray);
+            int k_size = 10;
+            for (String key : sortedmap.keySet()) {
+                VisionDetRet det = new VisionDetRet(key, sortedmap.get(key), 0, 0, 0, 0);
+                ret.add(det);
+                if (k_size == ret.size())
+                    break;
+            }
+        }
+        return ret;
+    }
+
+    /**
+     * Recognize the scene according to the given bitmap
+     *
+     * @param bitmap bitmap object {@link android.graphics.Bitmap} graphic object.
+     * @return The list of the result {@link VisionDetRet} which scenes are recognized
+     * @throws IllegalArgumentException if the Bitmap dimensions don't match
+     *                                  the dimensions defined at initialization or the given array
+     *                                  is not sized equal to the width and hegiht defined at initialization
+     */
+    @Override
+    public List<VisionDetRet> classify(Bitmap bitmap) {
+        storeBitmap(bitmap);
+
+        List<VisionDetRet> ret = new ArrayList<VisionDetRet>();
+        float[] propArray = jniClassifyBitmap(_handler);
+        if (propArray != null) {
+            Map<String, Float> sortedmap = Utils.sortPrediction(mSynsets, propArray);
+            int k_size = 10;
+            for (String key : sortedmap.keySet()) {
+                VisionDetRet det = new VisionDetRet(key, sortedmap.get(key), 0, 0, 0, 0);
+                ret.add(det);
+                if (k_size == ret.size())
+                    break;
+            }
+        }
+
+        freeBitmap();
+        return ret;
+    }
+
+    @Override
+    public void init(int imgWidth, int imgHeight) {
+        super.init(imgWidth, imgHeight);
+        jniLoadModel(mModelPath, mWeightsPath);
+        jniSetInputModelDim(MODEL_DIM, MODEL_DIM);
+    }
+
+    @Override
+    public void deInit() {
+        super.deInit();
+        jniRelease();
+    }
+
+    private void storeBitmap(final Bitmap bitmap) {
+        if (_handler != null)
+            freeBitmap();
+        _handler = jniStoreBitmapData(bitmap);
+    }
+
+    private void freeBitmap() {
+        if (_handler == null)
+            return;
+        jniFreeBitmapData(_handler);
+        _handler = null;
+    }
+
+    protected native int jniLoadModel(String modelPath, String weightsPath);
+
+    protected native int jniSetInputModelDim(int width, int height);
+
+    protected native int jniRelease();
+
+    protected native float[] jniClassifyImgByPath(String imgPath);
+
+    private native float[] jniClassifyBitmap(ByteBuffer handler);
+
+    // Bitmap
+    private native ByteBuffer jniStoreBitmapData(Bitmap bitmap);
+
+    private native void jniFreeBitmapData(ByteBuffer handler);
+
+    private native Bitmap jniGetBitmapFromStoredBitmapData(ByteBuffer handler);
+}
---
+++ cnnlibs/src/main/java/com/tzutalin/vision/visionrecognition/VisionClassifierCreator.java
@@ -0,0 +1,52 @@
+/*
+*  Copyright (C) 2015 TzuTaLin
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package com.tzutalin.vision.visionrecognition;
+
+import android.content.Context;
+
+import java.io.File;
+
+/**
+ * Create an instance using default instances for vision recognition and detection
+ */
+public class VisionClassifierCreator {
+    private final static String SCENE_MODEL_PATH = "/sdcard/vision_scene/mit/deploy_places205_mem.protxt";
+    private final static String SCENE_WIEGHTS_PATH = "/sdcard/vision_scene/mit/googlelet_places205_train_iter_2400000.caffemodel";
+    private final static String SCENE_MEAN_FILE = null;
+    private final static String SCENE_SYNSET_FILE = "/sdcard/vision_scene/mit/mit_category_table";
+
+    private final static String DETECT_MODEL_PATH = "/sdcard/fastrcnn/deploy.prototxt";
+    private final static String DETECT_WIEGHTS_PATH = "/sdcard/fastrcnn/caffenet_fast_rcnn_iter_40000.caffemodel";
+    private final static String DETECT_MEAN_FILE = "/sdcard/fastrcnn/imagenet_mean.binaryproto";
+    private final static String DETECT_SYNSET_FILE = "/sdcard/fastrcnn/fastrcnn_synset";
+
+    /**
+     * Create an instance using a default {@link SceneClassifier} instance
+     * @return {@link SceneClassifier instance
+     */
+    public static SceneClassifier createSceneClassifier(Context context) throws IllegalAccessException {
+        return new SceneClassifier(context, SCENE_MODEL_PATH, SCENE_WIEGHTS_PATH, SCENE_MEAN_FILE, SCENE_SYNSET_FILE);
+    }
+
+    /**
+     * Create an instance using a default {@link ObjectDetector} instance
+     * @return {@link ObjectDetector} instance
+     */
+    public static ObjectDetector createObjectDetector(Context context) throws IllegalAccessException {
+        return new ObjectDetector(context, DETECT_MODEL_PATH, DETECT_WIEGHTS_PATH, DETECT_MEAN_FILE, DETECT_SYNSET_FILE);
+    }
+}
