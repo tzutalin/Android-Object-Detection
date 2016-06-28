2db69d9ec428d4dd6b4d6314375c9e150d980b0a
tzutalin <tzu.ta.lin@gmail.com>
2016-06-28 20:50:26 +0800

    Add permission request for M, and add travis

--- app/src/main/java/com/tzutalin/vision/demo/CameraActivity.java
+++ app/src/main/java/com/tzutalin/vision/demo/CameraActivity.java
@@ -15,21 +15,78 @@
 */
 package com.tzutalin.vision.demo;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.widget.Toast;
 
 import com.tzutalin.vision.visionrecognition.R;
 
 public class CameraActivity extends Activity {
 
+    private static final int REQUEST_CODE_PERMISSION = 1;
+
+    private static final String TAG = "CameraActivity";
+
+    // Storage Permissions
+    private static String[] PERMISSIONS_REQ = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.CAMERA
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
-        if (null == savedInstanceState) {
+
+        boolean avialbe_permission = true;
+        // For API 23+ you need to request the read/write permissions even if they are already in your manifest.
+        int currentapiVersion = android.os.Build.VERSION.SDK_INT;
+        if (currentapiVersion >= Build.VERSION_CODES.M ) {
+            avialbe_permission = verifyPermissions(this);
+        }
+
+        if (avialbe_permission && null == savedInstanceState) {
             getFragmentManager().beginTransaction()
                     .replace(R.id.container, Camera2BasicFragment.newInstance())
                     .commit();
+        }
+    }
+
+    private static boolean verifyPermissions(Activity activity) {
+        // Check if we have write permission
+        int write_permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        int read_persmission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        int camera_permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.CAMERA);
+
+        if (write_permission != PackageManager.PERMISSION_GRANTED ||
+                read_persmission != PackageManager.PERMISSION_GRANTED ||
+                camera_permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSIONS_REQ,
+                    REQUEST_CODE_PERMISSION
+            );
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        try {
+            // Restart it after granting permission
+            if (requestCode == REQUEST_CODE_PERMISSION) {
+                finish();
+                startActivity(getIntent());
+            }
+        } catch (Exception e) {
+            Toast.makeText(this, "Something went wrong", Toast.LENGTH_LONG).show();
         }
     }
 }
--- setup.sh
+++ setup.sh
@@ -2,11 +2,14 @@
 
 if [ -d phone_data/ ]
 then
-    echo "model exists"
+    echo ".. model exists already ..."
 else
+    echo ".. Try to download the model ..."
     ./tools/get_model.py
 fi
 
+rm phone_data.tar > /dev/null 2>&1
+echo "--- Try to push data to sdcard ---"
+adb wait-for-device
 adb push phone_data/ /sdcard/
-rm phone_data.tar
 
